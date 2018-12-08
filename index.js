f7988b83a428c47c9f97343531c25d6ec9c49f34
bamboo123 <bamboo123love@163.com>
2018-12-08 20:42:51 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function zhuhaododo1(param) {
+    console.log('log1: ',param);
+}
+function zhuhaododo2(param) {
+    console.log('log2: ',param);
+}
+exports.zhuhaododo={
+    zh1:zhuhaododo1,
+    zh2:zhuhaododo2
+};
